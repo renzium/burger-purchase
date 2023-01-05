4ec840df627920919ac45cfd9ea6031eef6f5d79
Lawrence Ughonu <lawrenceughonu@gmail.com>
2023-01-05 16:08:05 +0100

    Add build file

---
+++ build/precache-manifest.7d81af8c0517d1029ec7a8c8078f661e.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e4a0d834185fe8dde72d2024581c9c2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "94567f6ae8267aac04d6",
+    "url": "/static/css/3.79cb56f3.chunk.css"
+  },
+  {
+    "revision": "763dd38574e9be771d99",
+    "url": "/static/css/4.157dfab5.chunk.css"
+  },
+  {
+    "revision": "bdefd049fa487adfb1ca",
+    "url": "/static/css/5.0c143387.chunk.css"
+  },
+  {
+    "revision": "06005cd1723d5cec5778",
+    "url": "/static/css/main.5a5c2d04.chunk.css"
+  },
+  {
+    "revision": "af3abe30d013c8397b49",
+    "url": "/static/js/2.ac0f56a1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.ac0f56a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "94567f6ae8267aac04d6",
+    "url": "/static/js/3.045d7286.chunk.js"
+  },
+  {
+    "revision": "763dd38574e9be771d99",
+    "url": "/static/js/4.97ced7ba.chunk.js"
+  },
+  {
+    "revision": "bdefd049fa487adfb1ca",
+    "url": "/static/js/5.cfbd8df2.chunk.js"
+  },
+  {
+    "revision": "06005cd1723d5cec5778",
+    "url": "/static/js/main.268b6c4d.chunk.js"
+  },
+  {
+    "revision": "eb8d41e47e4ed058ca4b",
+    "url": "/static/js/runtime-main.d0896d7b.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/static/media/burger-logo.b8503d26.png"
+  }
+]);
